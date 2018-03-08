efdbb45feae2b6be0d6e7a854906783f29d09605
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-08 13:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
-return customerName.toUpperCase();
+customerName = customerName.toUpperCase();
 };
 
 function setBestCustomer() {
