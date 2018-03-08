ac65246952755cc10721635b661a93e8f882315d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-08 13:07:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'sam';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(copy) {
+  bestCustomer = copy;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob';
+}
