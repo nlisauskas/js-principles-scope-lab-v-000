681f03542d80db1a98e746b66eb3394d29de154d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-08 13:00:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 const customerName = 'bob';
 
-function upperCaseCustomerName(name) {
-return name.toUpperCase();
+function upperCaseCustomerName() {
+return customerName.toUpperCase();
 }
